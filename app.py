f7675dae6d4290ba18ab8709c3d519a83a912b04
Jose Garcia Ponce <jose.garciaponce@salesforce.com>
2021-10-21 16:11:48 +0100

    changes from Naveen

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import getopt
 import os
 import random
+import sys
 import time
 from urllib.parse import urlparse
 
@@ -7,29 +9,33 @@ from flask import Flask, request, session, flash, redirect, \
     url_for, jsonify
 from celery import Celery
 
+from werkzeug.utils import secure_filename
+
+import locustExtract
+
 app = Flask(__name__)
 
 # Celery configuration
 
-app.config['CELERY_BROKER_URL'] = os.getenv('REDISCLOUD_URL') if os.getenv('REDISCLOUD_URL') else 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = os.getenv('REDISCLOUD_URL') if os.getenv('REDISCLOUD_URL') else 'redis://localhost:6379/0'
-
+app.config['CELERY_BROKER_URL'] = os.getenv('REDISCLOUD_URL') if os.getenv(
+    'REDISCLOUD_URL') else 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = os.getenv('REDISCLOUD_URL') if os.getenv(
+    'REDISCLOUD_URL') else 'redis://localhost:6379/0'
 
 # Initialize Celery
-celery_app = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'], include=['app'])
+celery_app = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'],
+                    include=['app'])
 celery_app.conf.update(app.config)
+
+app.config['UPLOAD_FOLDER'] = os.getcwd()
 
 
 @celery_app.task(bind=True)
-def start_test_execution(self):
+def start_test_execution(self,yaml_location):
     """Background task that runs a long function with progress reports."""
-    print('Starting test')
-
-    total = random.randint(10, 50)
+    locustExtract.scriptentrypoint(os.path.abspath(yaml_location))
     print('executed long task')
-    time.sleep(15)
-    return {'current': 100, 'total': 100, 'status': 'Task completed!',
-            'result': total}
+    return {'status': 'Task completed!'}
 
 
 @app.route('/', methods=['GET'])
@@ -40,9 +46,14 @@ def go_home():
     return jsonify(response)
 
 
-@app.route('/start', methods=['POST','GET'])
+@app.route('/start', methods=['POST', 'GET'])
 def start_test():
-    task = start_test_execution.apply_async()
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        yaml_location = f.filename
+        print('file uploaded successfully')
+    task = start_test_execution.apply_async(args=[yaml_location])
     print('called test task method')
     return jsonify({}), 202, {'Location': url_for('test_task_status',
                                                   task_id=task.id)}
@@ -81,4 +92,15 @@ def test_task_status(task_id):
 
 
 if __name__ == '__main__':
+    # argumentList = sys.argv[1:]
+    # options = "i:"
+    # long_options = ["input="]
+    # arguments, values = getopt.getopt(argumentList, options, long_options)
+    # if len(arguments) == 0:
+    #     sys.exit("No arguments given hence terminated!!")
+    # for currentArgument, currentValue in arguments:
+    #     if currentArgument in ("-i", "--input"):
+    #         yaml_location = currentValue
+    #     else:
+    #         sys.exit("[ERROR] Please provide valid config file!!")
     app.run(port=5000, debug=False)
